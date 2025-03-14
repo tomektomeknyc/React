90d61601b2edfa107c687f705279c9a7ee4178cb
Tomek Osuchowski <tomektomeknyc@yahoo.com>
2025-03-14 14:42:00 +0000

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-//App functionn
+//App functionnn
 export default function App() {
   const [advice, setAdvice] = useState("");
   const [count, setCount] = useState(0);
